5db5098d49c086de5522b4ba0343a1ff58130c12
thiagorochatr <thiagord2016@gmail.com>
2024-02-29 19:54:21 -0300

    create folder aula2

---
+++ aula2/extenso.php
@@ -0,0 +1,25 @@
+<?php
+
+function extenso($dia, $mes, $ano) {
+    $meses = [
+        1 => 'janeiro', 2 => 'fevereiro', 3 => 'março',
+        4 => 'abril', 5 => 'maio', 6 => 'junho',
+        7 => 'julho', 8 => 'agosto', 9 => 'setembro',
+        10 => 'outubro', 11 => 'novembro', 12 => 'dezembro'
+    ];
+
+    if ($dia < 1 || $dia > 31 || $mes < 1 || $mes > 12 || $ano < 0) {
+        return 'Data inválida';
+    }
+
+    $dataExtenso = $dia . ' de ' . $meses[$mes] . ' de ' . $ano;
+
+    return $dataExtenso;
+}
+
+$dia = 28;
+$mes = 2;
+$ano = 2024;
+
+echo extenso($dia, $mes, $ano);
+?>
---
+++ aula2/meses.php
@@ -0,0 +1,13 @@
+<?php
+
+$meses = [
+    1 => 'janeiro', 2 => 'fevereiro', 3 => 'março',
+    4 => 'abril', 5 => 'maio', 6 => 'junho',
+    7 => 'julho', 8 => 'agosto', 9 => 'setembro',
+    10 => 'outubro', 11 => 'novembro', 12 => 'dezembro'
+];
+
+foreach ( $meses as $indice => $mes ) {
+    echo "Número: $indice, Nome: $mes\n";
+}
+?>
