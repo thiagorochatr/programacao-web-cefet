33557699a636d98d29ae656b8d9b0b75524b73da
thiagorochatr <thiagord2016@gmail.com>
2024-02-29 21:14:28 -0300

    update aula2

--- aula2/media.php
+++ aula2/media.php
@@ -1,28 +1,32 @@
 <?php
 
 function calcularMedia() {
-    $numeros = [];
+  $numeros = [];
 
-    echo "Digite números (digite 0 para calcular a média):\n";
+  echo "Digite números (digite 0 para calcular a média):\n";
     
-    while (true) {
-        $num = (float) readline("Número: ");
-        
-        if ($num == 0) {
-            // Calcular a média quando 0 é inserido
-            if (count($numeros) === 0) {
-                echo "Nenhum número foi digitado.\n";
-            } else {
-                $media = array_sum($numeros) / count($numeros);
-                echo "Números digitados: " . implode(", ", $numeros) . "\n";
-                echo "Média dos números: $media\n";
-            }
-            break;
+  while (true) {
+    $num = (float) readline("Número: ");
+    
+    if(is_numeric($num)) {
+      if ($num == 0) {
+        // Calcular a média quando 0 é inserido
+        if (count($numeros) === 0) {
+          echo "Nenhum número foi digitado.\n";
         } else {
-            // Adicionar número ao array
-            $numeros[] = $num;
+          $media = array_sum($numeros) / count($numeros);
+          echo "Números digitados: " . implode(", ", $numeros) . "\n";
+          echo "Média dos números: $media\n";
         }
+        break;
+      } else {
+        // Adicionar número ao array
+        $numeros[] = $num;
+      }
+    } else {
+      break;
     }
+  }
 }
 
 // Chamar a função
