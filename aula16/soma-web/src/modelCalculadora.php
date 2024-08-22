09fb4fd5934e61e376fe8241d0eaaaf4030a23aa
thiagorochatr <thiagord2016@gmail.com>
2024-08-22 20:51:35 -0300

    a

--- aula16/soma-web/src/modelCalculadora.php
+++ aula16/soma-web/src/modelCalculadora.php
@@ -10,6 +10,27 @@ class ModelCalculadora {
     }
     return $x + $y;
   }
+
+  function dividir (string $x, string $y): int|float {
+    if(!is_numeric($x)) {
+      throw new RuntimeException('O primeiro número precisa ser numérico');
+    }
+    if(!is_numeric($y)) {
+      throw new RuntimeException('O segundo número precisa ser numérico');
+    }
+    if($y == '0') {
+      throw new RuntimeException('Não pode dividir por 0');
+    }
+    return $x/$y;
+  }
+
+  function realizarOperacao(string $operacao, string $x, string $y) {
+    if($operacao == 'dividir') {
+      return $this->dividir($x,$y);
+    } else if($operacao == 'somar') {
+      return $this->somar($x,$y);
+    }
+  }
 }
 
 ?>
