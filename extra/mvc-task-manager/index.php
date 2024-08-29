420dfc10c8785d1b9b8436a9e8c53ce7654d34f4
thiagorochatr <thiagord2016@gmail.com>
2024-08-29 03:43:27 -0300

    finish (without update)

--- extra/mvc-task-manager/index.php
+++ extra/mvc-task-manager/index.php
@@ -19,39 +19,43 @@ $url = $_SERVER['REQUEST_URI'];
 
 $controller = new TaskController();
 
-if ($metodo === 'GET') {
+if (mb_strpos($url, '/tasks') !== false) {
 
-  if (mb_strpos($url, '/tasks')) {
+  if ($metodo === 'GET') {
+
     http_response_code(200);
     $controller->create();
     $controller->index();
-  } else {
-    http_response_code(404);
-    die ('Não encontrado.');
-  }
 
-} else if ($metodo === 'POST') {
+  } else if ($metodo === 'POST') {
 
-  if (mb_strpos($url, '/tasks') && !mb_strpos($url, '/update.php') && (!$_POST['_method'] === 'DELETE' || !isset($_POST['_method']))) {
-    $controller->store();
-    http_response_code(302); // Redirect status code
-    header('Location: index.php/tasks');
-    exit();
-  } else if(mb_strpos($url, '/tasks') && $_POST['_method'] === 'DELETE') {
-    $controller->delete();
-    http_response_code(302); // Redirect status code
-    header('Location: index.php/tasks');
-    exit();
-  }else if (mb_strpos($url, '/update.php')) {
-    echo '<p>Update</p>';
-  } else {
-    http_response_code(404);
-    die ('Não encontrado.');
+    if (!isset($_POST['_method'])) {
+
+      $controller->store();
+      http_response_code(302); // Redirect status code
+      header('Location: index.php/tasks');
+      exit();
+
+    } else if ($_POST['_method'] === 'DELETE') {
+      
+      $controller->delete();
+      http_response_code(302); // Redirect status code
+      header('Location: index.php/tasks');
+      exit();
+
+    } else if ($_POST['_method'] === 'UPDATE') {
+      
+      echo '<p>Update</p>';
+
+    }
+
   }
 
 } else {
+
   http_response_code(404);
   die ('Não encontrado.');
+
 }
 
 ?>
